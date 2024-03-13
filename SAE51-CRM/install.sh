b8fd5e7f051f28f1269f998bc04c3097b9b9796b
PierreHeb <144241779+PierreHeb@users.noreply.github.com>
2024-03-13 11:21:33 +0100

    Add files via upload

--- SAE51-CRM/install.sh
+++ SAE51-CRM/install.sh
@@ -1,6 +1,7 @@
 docker compose up -d
-docker run dokku/wait -c crm-bdd:3306 
+docker run --name wait dokku/wait -c crm-bdd:3306  
 docker cp save_bdd.sh crm-bdd:/save_bdd.sh
 docker cp users.csv crm-bdd:/users.csv
 docker cp insertion_data.sh crm-bdd:/insertion_data.sh
 docker exec -it crm-bdd /insertion_data.sh
+docker rm wait
